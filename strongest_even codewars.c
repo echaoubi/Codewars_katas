ff421b1af760b6cc0c76e1176f97f43b849b1579
abdochaoubi <52721022+abdochaoubi@users.noreply.github.com>
2022-02-25 12:44:43 +0100

    Create strongest_even codewars.c

---
+++ strongest_even codewars.c
@@ -0,0 +1,21 @@
+
+int strongest_even(unsigned long long int n, unsigned long long int m) {
+  unsigned long long int p=1;
+  while(p<=m) p*=2;
+  p/=2;
+  if(p>=n){
+    return p;}
+  else {
+    p=p/2;
+    int i=1;
+    for(;p*i<n;){ 
+      i++;
+      if(p*i>m) p/=2;}
+    return p*i;
+  }
+}
+/*
+int strongest_even(int n, int m) {
+  for (; (m & m - 1) >= n; m &= m - 1);
+  return m;
+}*/

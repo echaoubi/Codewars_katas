ed4881a4c00ee11a5c8334259f820d7fbb57495f
abdochaoubi <52721022+abdochaoubi@users.noreply.github.com>
2022-03-19 12:48:25 +0100

    Update Rail Fence Cipher: Encoding and Decodingcodewar.py

--- Rail Fence Cipher: Encoding and Decodingcodewar.py
+++ Rail Fence Cipher: Encoding and Decodingcodewar.py
@@ -1,31 +1,29 @@
 def encode_rail_fence_cipher(str, n):
-    txt=[""]
+    txt=[]
     for i in(range(n)):
         txt.append("")
-    pw=0
-    a=0
+    pw,a=0,0
     for i in range(0,len(str)):
         txt[pw]+=str[i]
-        if(pw==0):a=0
-        if(pw==n-1):a=1
-        if(a):pw-=1
-        else:pw+=1
-    return("".join(txt))
-    
-    
-    
-    
-def decode_rail_fence_cipher(str, n):
-    txt=[""]
-    for i in(range(n)):
-        txt.append("")
-    pw=0
-    a=0
-    for i in range(0,len(str)):
-        txt[pw]+=str[i]
-        if(pw==0):a=0
-        if(pw==n-1):a=1
-        if(a):pw-=1
-        else:pw+=1
-    print(txt)
-    return("".join(txt))
+        a=0 if(pw==0) else 1 if(pw==n-1) else a
+        pw=pw-1 if(a)  else pw+1
+    return("".join(txt))    
+
+
+
+def decode_rail_fence_cipher(strt, n):
+    txt,kak=[],[]
+    pw,a,tr=0,0,0
+    for i in range(0,len(strt)):
+        txt.append(str(pw))
+        a=0 if(pw==0) else 1 if(pw==n-1) else a
+        pw=pw-1 if(a)  else pw+1
+    for i in(range(len(strt))):
+        kak.append("")
+    for rt in range(n):
+            indices = [i for i, x in enumerate(txt) if x == str(rt)]
+            print(indices)
+            for y in indices:
+                kak[y]=strt[tr]
+                tr+=1
+    return("".join(kak))
